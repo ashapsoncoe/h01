28cb4f30c911098c202e5e22ea914ef290921356
ashapsoncoe <31902042+ashapsoncoe@users.noreply.github.com>
2023-07-06 23:01:39 +0100

    Update get_split_and_merge_stats_for_proofread_neurons.py

--- get_split_and_merge_stats_for_proofread_neurons.py
+++ get_split_and_merge_stats_for_proofread_neurons.py
@@ -12,7 +12,7 @@ from google.oauth2 import service_account
 import pandas as pd
 
 
-proofread_cells_dir = 'proofread104_neurons_20210511'
+proofread_cells_dir = 'proofread104_neurons_20210511' # available at https://storage.cloud.google.com/h01_paper_public_files/proofread104_neurons_20210511.zip
 cred_path = 'alexshapsoncoe.json' # or your credentials file 
 output_file_name = 'proofread_neurons_split_and_merge_stats.csv'
 
